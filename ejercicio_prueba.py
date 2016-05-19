efe7a7ffbaf9563fdfd46ce5bc1dd85710f6c7dd
Mario Jose Nuñez Villacis <pepeseya@hotmail.com>
2016-05-19 08:41:16 -0500

    prueba
    implemantado parte 1

--- ejercicio_prueba.py
+++ ejercicio_prueba.py
@@ -1,3 +1,19 @@
+def lista1():
+	x=[0,0,0]
+	for i in range (len(x)):
+		x[i]=int(input('ingrese numero vector 1: '))
+	return x;
+def lista2():
+	y=[0,0,0]
+	for i in range (len(x)):
+		y[i]=int(input('ingrese numero vector2: '))
+	return y;
+def suma(x,y):
+	r=[0,0,0]
+	for i in range(len(r)):
+		r[i]=x[i]+y[i]
+	return r
+
 def ingresar():
 	continuar = 1
 	contador = 0
@@ -11,9 +27,18 @@ def ingresar():
 	promedio=suma/(contador-1)
 	return promedio
 print('------menu-----')
-print('1')
-print('2.-promedio de numeros')	
+print('1.-Suma de vectores')
+print('2.-Promedio de numeros')
+print('3.-Dividir un archivo')
+print('4.-Salir')
+print('________________')	
 opcion = int(input('ingrese una opcion: '))
+if(opcion==1):
+	x=lista1()
+	y=lista2()
+	print(x)
+	print(y)
+	print(suma(x,y))
 if(opcion==2):
 	valor=ingresar()
 	print (valor)
