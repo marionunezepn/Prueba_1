c49e993b0ea77a50fcecf7c7edb76c9beceecd33
oscarepv <oscar_e1993@hotmail.com>
2016-05-19 07:36:43 -0500

    Segundo
    implementacion del segundo ejercicio

--- ejercicio_prueba.py
+++ ejercicio_prueba.py
@@ -1,9 +1,19 @@
 def ingresar():
 	continuar = 1
 	contador = 0
+	suma = 0
 	while (continuar==1):
 		x=int(input('ingrese un numero: '))
+		suma=suma+x
 		if(x==0):
 			continuar=0
 		contador=contador+1
-ingresar()
+	promedio=suma/(contador-1)
+	return promedio
+print('------menu-----')
+print('1')
+print('2.-promedio de numeros')	
+opcion = int(input('ingrese una opcion: '))
+if(opcion==2):
+	valor=ingresar()
+	print (valor)
